fc6df0bee35921224fd60e156045ea41ae49a20d
nolan aguirre <nolanaguirre08@gmail.com>
2017-04-20 18:56:38 -0600

    add two basic files

---
+++ ControlStatements.java
@@ -0,0 +1,12 @@
+public class ControlStatement{
+    public static void main(String[] args){
+        int x = 8;
+        if(x == 8){
+            System.out.println("x is equal to 8");
+        }else if(x > 8){
+            System.out.println("x is greater than 8");
+        }else{
+            System.out.println("x is less than 8")
+        }
+    }
+}
---
+++ Loops.java
@@ -0,0 +1,19 @@
+public class Loops{
+    public static void main(Sting[] args){
+        int x = 0;
+        while(x < 10){
+            System.out.println(x);
+            x++;
+        }
+
+        int y = 0;
+        do{
+            System.out.println(x);
+            y++;
+        }while(y < 10);
+
+        for(int z = 0; z < 10; z++){
+            System.out.println(z);
+        }
+    }
+}
