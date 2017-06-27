963c8ca81b6327d4220907b6a4b449a596132613
nolan aguirre <nolanaguirre08@gmail.com>
2017-06-26 20:17:39 -0500

    add student lab projects

---
+++ Labs/Lab-1/Student/Lab1.java
@@ -0,0 +1,23 @@
+
+public class Lab1 {
+	public static void main(String[] args){
+		//int[][] number= new int [13][13];
+		
+		int x=0;
+		int y=0;
+		for(x=0 ;x<13;x++){
+			
+		for(y=0 ;y<13;y++) {
+			
+		System.out.print(x*y + " ");
+		}
+		
+		System.out.println("");}
+		
+		
+		 
+		
+		
+	} 
+
+}
