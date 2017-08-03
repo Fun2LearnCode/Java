41f758d3636ae18477d484271fa9751a014ac53e
nolanaguirre <nolanaguirre08@gmail.com>
2017-08-03 16:01:25 -0500

    add lab 5

---
+++ labs/lab-5/README.md
@@ -0,0 +1,7 @@
+# Lab 5
+
+Lab 5 is meant to be an easy but repetitive lab. The goal of this lab is to understand what each method type and the terminology used.
+
+There are examples of each method and the class variable in the example folder.
+
+In order to understand each, and know how to write each of these a class with a class variable of each datatype, with getter and setter methods, will be made. 
---
+++ labs/lab-5/example/Lab5.java
@@ -0,0 +1,15 @@
+public class Lab5{
+    private int count; // this is a class variable, aslo known as an instance variable, or a field
+    public Lab5(){ // this is a constructor
+        count = 5;
+    }
+    public void setCount(int count){ // this is a setter
+        this.count = count;
+    }
+    public int getCount(){ // this is a getter
+        return count;
+    }
+    public static void main(String args[]){ // this is the main method
+
+    }
+}
