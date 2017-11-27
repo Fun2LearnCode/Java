bcd2e61a4906e667bb74517a939f58fa17aa197d
Nolan Aguirre <nolanaguirre08@gmail.com>
2017-11-27 17:47:13 -0600

    add lab 14

---
+++ labs/lab-14/README.md
@@ -0,0 +1,5 @@
+# Lab 14
+
+Lab 14 is a lab about method signatures. The book covers all of the required and optional aspects of a method signature, and they must just be memorized.
+
+Because this topic is purely memorization, the lab is to simply make 12 unique method signatures. Examples of what counts an a unique method signature counts as is show in the example Java folder.
---
+++ labs/lab-14/example/MethodSignature.java
@@ -0,0 +1,17 @@
+public class MethodSignature{
+    public static void main(String args[]){
+
+    }
+
+    // two unique method signatures are shown below
+    public static void methodOne(){}
+
+    private int methodTwo(){}
+    // these two methods differ in at least two ways, excluding the name differance
+
+    // two methods that are not unique enough are shown below
+    public int methodThree(){}
+
+    public int methodFour(int arg1){}
+    // these two methods only differ in one way, excluding the name
+}
